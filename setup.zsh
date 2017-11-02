39313db69f00cc3694d532374fa7906182c07f9d
Jamon Holmgren <jamon@infinite.red>
2017-11-02 11:47:17 -0700

    Adds exa

--- setup.zsh
+++ setup.zsh
@@ -5,7 +5,7 @@ setup_jamon_environment () {
   # homebrew
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   # zsh and lots of other cool things
-  brew install --without-docs zsh zsh-completions zsh-syntax-highlighting ccat ripgrep trash
+  brew install --without-docs zsh zsh-completions zsh-syntax-highlighting ccat ripgrep exa trash
   # oh-my-zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
   # pureprompt
