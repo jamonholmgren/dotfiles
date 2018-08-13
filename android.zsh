52f9d50610a0f48fed8e5025d2923e024d2c32a9
Jamon Holmgren <jamon@infinite.red>
2018-08-12 19:23:02 -0700

    Android

--- android.zsh
+++ android.zsh
@@ -2,6 +2,8 @@ alias ae="emulator -netdelay none -netspeed full -avd Nexus_5X_API_25_x86"
 
 # React Native Android dev
 export ANDROID_HOME=$HOME/Library/Android/sdk
+export ANDROID_SDK=$ANDROID_HOME
+# export ANDROID_NDK=$ANDROID_HOME/ndk/android-ndk-r10e
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
