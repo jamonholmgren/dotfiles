84203bd642ad2ffd45ce5a3009e32c26daf0fc9e
Jamon Holmgren <jamon@infinite.red>
2019-05-07 12:58:37 -0700

    add nice to rn commands

--- xcode.zsh
+++ xcode.zsh
@@ -1,8 +1,8 @@
 alias xcode="open -a Xcode *.xcworkspace"
 
-alias rn="react-native"
-alias rni="react-native run-ios"
-alias rnr="react-native run-android"
-alias rnpkg="./node_modules/react-native/packager/launchPackager.command ; exit;"
+alias rn="nice -n 5 react-native"
+alias rni="nice -n 5 react-native run-ios"
+alias rnr="nice -n 5 react-native run-android"
+alias rnpkg="nice -n 5 ./node_modules/react-native/packager/launchPackager.command ; exit;"
 
 export PATH=$PATH:/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin
