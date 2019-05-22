d92049fa97dfa8f7cfb3bafab02b6e38b644c223
Jamon Holmgren <jamon@infinite.red>
2019-05-22 11:11:58 -0700

    Updated pixel2

--- android.zsh
+++ android.zsh
@@ -1,5 +1,3 @@
-alias pixel2="emulator -netdelay none -netspeed full -avd Pixel_2_API_28"
-
 # React Native Android dev
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export ANDROID_SDK=$ANDROID_HOME
@@ -7,3 +5,5 @@ export ANDROID_SDK=$ANDROID_HOME
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+alias pixel2="$ANDROID_HOME/emulator/emulator -netdelay none -netspeed full -no-snapshot-save -avd Pixel_2_API_28"
