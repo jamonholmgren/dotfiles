48ba31237ed2c15dbb295af6104ef8927a6dd4bd
Jamon Holmgren <jamonholmgren@gmail.com>
2020-01-24 12:44:14 -0800

    Updates

--- xcode.zsh
+++ xcode.zsh
@@ -1,8 +1,3 @@
 alias xcode="open -a Xcode *.xcworkspace"
 
-alias rn="nice -n 5 react-native"
-alias rni="nice -n 5 react-native run-ios"
-alias rnr="nice -n 5 react-native run-android"
-alias rnpkg="nice -n 5 ./node_modules/react-native/packager/launchPackager.command ; exit;"
-
 export PATH=$PATH:/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin
